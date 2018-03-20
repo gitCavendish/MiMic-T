ecb5e2681ae6564eb1580a9716e8162ec7798cbc
gitCavendish <xll290016537@gmail.com>
2018-03-20 09:46:44 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params) # 非最终版本
+    if @user.save
+      flash[:success] = "Welcome to Sample App!" # 这里的flash content实际是为后面view中用到作准备
+      redirect_to @user # or user_url(@user)
+    else
+      render "new"
+    end
+  end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
