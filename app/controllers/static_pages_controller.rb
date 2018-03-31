795d7c25416aa5b685feff2a3273ff08591557a7
gitCavendish <xll290016537@gmail.com>
2018-03-31 10:49:20 +0800

    ready for camp

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,9 +6,9 @@ class StaticPagesController < ApplicationController
   def home
     if current_user
       @micropost = current_user.microposts.new if current_user
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 8)
     else
-      @feed_items = Micropost.all.sample(15).paginate(page: params[:page])
+      @feed_items = Micropost.all.sample(15).paginate(page: params[:page], :per_page => 8)
     end
   end
 
