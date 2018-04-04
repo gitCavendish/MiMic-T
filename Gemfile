1ef789680f1e77687929ed0b3b8a176f126eb0c3
gitCavendish <xll290016537@gmail.com>
2018-04-04 16:00:19 +0800

    setup rspec

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'rspec-rails'
+  gem 'guard-rspec'
 end
 
 group :development do
@@ -58,6 +60,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,20 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     # 在使用 Ajax 处理的表单中添加真伪令牌
     config.action_view.embed_authenticity_token_in_remote_forms =  true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
   end
 end
